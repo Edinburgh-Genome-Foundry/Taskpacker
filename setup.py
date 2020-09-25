77b47ba2b2da5847ceee75d39f66daa620111b40
Peter Vegh <pvegh@posteo.uk>
2020-09-25 14:20:36 +0100

    License

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('taskpacker/version.py').read()) # loads __version__
+exec(open("taskpacker/version.py").read())  # loads __version__
 
 setup(
-    name='taskpacker',
+    name="taskpacker",
     version=__version__,
-    author='Valentin',
-    description='',
-    long_description=open('README.rst').read(),
-    license='MIT',
+    author="Valentin",
+    description="",
+    long_description=open("README.rst").read(),
+    license="MIT",
     keywords="",
-    packages=find_packages(exclude='docs'),
-    install_requires=['Numberjack', 'numpy', 'tqdm', 'xlrd', 'pandas',
-                      'matplotlib'])
+    packages=find_packages(exclude="docs"),
+    install_requires=["Numberjack", "numpy", "tqdm", "xlrd", "pandas", "matplotlib"],
+)
